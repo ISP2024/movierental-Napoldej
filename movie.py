6798609a7851ae251edf370f585488a16b3c8490
Napoldej Passornratchakul <napoldej.p@ku.th>
2024-10-19 14:10:16 +0700

    Revised movie class into dataclass and add attributes

--- movie.py
+++ movie.py
@@ -1,20 +1,25 @@
-from pricing import RegularPrice, NewRelease, ChildrensPrice
+from typing import Collection, List
+from dataclasses import dataclass
 
+@dataclass(frozen= True)
 class Movie:
     """
-    A movie available for rent.
+    A movie available for rent, with title, year, and genre.
+    Immutable dataclass for Movie.
     """
-    # The types of movies (price_code).
+    title : str
+    year : int
+    genre : Collection[str]
 
-    def __init__(self, title):
-        # Initialize a new movie.
-        self.title = title
+    def is_genre(self, genre_name: str) -> bool:
+        """
+        Check if the movie's genre matches the provided genre name.
+        Case insensitive.
+        """
+        return genre_name.lower() in (g.lower() for g in self.genre)
 
-
-    def get_title(self):
-        return self.title
-
-    def __str__(self):
-        return self.title
-
-
+    def __str__(self) -> str:
+        """
+        Return a string representation of the Movie in the format "Title (year)".
+        """
+        return f"{self.title} ({self.year})"
